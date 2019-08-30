846250058ed0689fa0b7d33abf0e4956c81d5bb6
SetoAira <setoaira@msn.com>
2019-08-30 22:39:30 +0900

    目次ページ　作成

---
+++ _plugins/myfilter.rb
@@ -0,0 +1,24 @@
+module Jekyll
+  module MyFilter
+    
+    def toc(html, fname = "001.xhtml")
+      i = 0
+      toc_list = +''
+
+      html.scan(/<h[1-6].*?<\/h[1-6]>/) do |header|
+        node_name = header.match(/h[1-6]/)[0]
+        linkstr = header.sub(/<h[1-6] id="(.*?)<\/h[1-6]>/, '\1</a>')
+        toc_list << %(    <li class="toc-#{node_name}"><a href="#{fname}##{linkstr}</li>\n)
+        i += 1
+      end
+
+      toc_list
+      
+    end
+    
+  end
+  
+  
+end
+
+Liquid::Template.register_filter(Jekyll::MyFilter)
