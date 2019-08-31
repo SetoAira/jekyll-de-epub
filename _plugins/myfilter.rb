613859fcd1ce71717126f06df05d6157d4901b83
SetoAira <setoaira@msn.com>
2019-08-31 16:42:12 +0900

    ナビゲーション作成

--- _plugins/myfilter.rb
+++ _plugins/myfilter.rb
@@ -16,6 +16,27 @@ module Jekyll
       
     end
     
+    def nav(html, fname = "xhtml/001.xhtml")
+      i = 0
+      toc_list = +''
+
+      html.scan(/<h[1-6].*?<\/h[1-6]>/) do |header|
+        node_num = header.match(/[1-6]/)[0].to_i
+        toc_space = +''
+        j = 1
+        while j < node_num
+          toc_space << ' '
+          j += 1
+        end
+        linkstr = header.sub(/<h[1-6] id="(.*?)>(.*?)<\/h[1-6]>/, '\1>'+toc_space+'\2</a>')
+        toc_list << %(      <li><a href="#{fname}##{linkstr}</li>\n)
+        i += 1
+      end
+
+      toc_list
+      
+    end
+
   end
   
   
